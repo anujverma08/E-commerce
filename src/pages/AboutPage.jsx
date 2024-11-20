51a47e777cddc57fab24c7fe6efba2c6e585edc6
Anuj Verma <anujvermagkp@gmail.com>
2024-11-20 10:54:49 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/assets/main.png.jpg 	new file:   public/assets/sdmain.png.jpg 	new file:   public/favicon.ico 	new file:   public/favicond.ico 	new file:   public/index.html 	new file:   public/robots.txt 	new file:   src/components/Footer.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/Products.jsx 	new file:   src/components/ScrollToTop.jsx 	new file:   src/components/index.js 	new file:   src/components/main.jsx 	new file:   src/index.js 	new file:   src/pages/AboutPage.jsx 	new file:   src/pages/Cart.jsx 	new file:   src/pages/Checkout.jsx 	new file:   src/pages/ContactPage.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/Login.jsx 	new file:   src/pages/PageNotFound.jsx 	new file:   src/pages/Product.jsx 	new file:   src/pages/Products.jsx 	new file:   src/pages/Register.jsx 	new file:   src/pages/index.js 	new file:   src/redux/action/index.js 	new file:   src/redux/reducer/handleCart.js 	new file:   src/redux/reducer/index.js 	new file:   src/redux/store.js

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Welcome to UrbanCraze, your ultimate destination for trendy, high-quality fashion and lifestyle essentials. Born from a passion for style and innovation, we bring you curated collections that combine urban vibes with contemporary elegance.
+        At UrbanCraze, we believe fashion is more than just clothing; it’s a way to express yourself and make a statement. Our commitment lies in delivering exceptional products that cater to every personality, ensuring you find something uniquely you.
+        From cutting-edge designs to timeless classics, we aim to redefine your shopping experience with unmatched quality, affordability, and service. Join us as we transform everyday wear into extraordinary style.
+        UrbanCraze – Your Style, Your Craze.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
